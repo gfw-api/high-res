68d587f4f8ca99532a02792940622c5e7a34a741
Tiago Garcia <tiagojsag@gmail.com>
2021-01-26 17:08:30 +0100

    Replace CT integration library

--- app/src/app.js
+++ app/src/app.js
@@ -3,7 +3,7 @@ const logger = require('logger');
 const koaLogger = require('koa-logger');
 const config = require('config');
 const loader = require('loader');
-const ctRegisterMicroservice = require('ct-register-microservice-node');
+const { RWAPIMicroservice } = require('rw-api-microservice-node');
 const ErrorSerializer = require('serializers/error.serializer');
 const koaSimpleHealthCheck = require('koa-simple-healthcheck');
 const koaBody = require('koa-body');
@@ -46,29 +46,32 @@ app.use(async (ctx, next) => {
 app.use(koaLogger());
 app.use(koaSimpleHealthCheck());
 
+app.use(RWAPIMicroservice.bootstrap({
+    name: config.get('service.name'),
+    info: require('../microservice/register.json'),
+    swagger: require('../microservice/public-swagger.json'),
+    logger,
+    baseURL: process.env.CT_URL,
+    url: process.env.LOCAL_URL,
+    token: process.env.CT_TOKEN,
+    fastlyEnabled: process.env.FASTLY_ENABLED,
+    fastlyServiceId: process.env.FASTLY_SERVICEID,
+    fastlyAPIKey: process.env.FASTLY_APIKEY
+}));
+
 loader.loadRoutes(app);
 
-
 const server = app.listen(process.env.PORT, () => {
-    ctRegisterMicroservice.register({
-        info: require('../microservice/register.json'),
-        swagger: require('../microservice/public-swagger.json'),
-        mode: (process.env.CT_REGISTER_MODE && process.env.CT_REGISTER_MODE === 'auto') ? ctRegisterMicroservice.MODE_AUTOREGISTER : ctRegisterMicroservice.MODE_NORMAL,
-        framework: ctRegisterMicroservice.KOA2,
-        app,
-        logger,
-        name: config.get('service.name'),
-        ctUrl: process.env.CT_URL,
-        url: process.env.LOCAL_URL,
-        token: process.env.CT_TOKEN,
-        active: true,
-    }).then(() => {
-    }, (err) => {
-        logger.error(err);
-        process.exit(1);
-    });
+    if (process.env.CT_REGISTER_MODE === 'auto') {
+        RWAPIMicroservice.register().then(() => {
+            logger.info('CT registration process started');
+        }, (error) => {
+            logger.error(error);
+            process.exit(1);
+        });
+    }
 });
-logger.info('Server started in ', process.env.PORT);
 
+logger.info('Server started in ', process.env.PORT);
 
 module.exports = server;
